289efdc23359bdc6ea8cb39cd916c839d449dca3
vethu24 <vethu24@gmail.com>
2020-12-06 10:49:29 +0530

    header,footer and login pages design

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,87 @@
+<div class="panel">
+  <div class="panel-heading custom-panel">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <h3 class="panel-title">SIGN UP FOR HOT DEALS</h3><br>
+          <p>Steady ahead of the pack with up to date information on the very latest camera products and special offers:</p>
+        </div>
+        <div class="col-sm-4">
+        <h3></h3><br>
+          <form class="input-group">
+              <input type="text"  placeholder="Enter Your email">
+               <input class="custom-submit" type="submit" value="SUBSCRIBE" />
+              
+          </form>
+        </div>
+        <div class="col-sm-4">
+           <h3  class="panel-title">FOLLOW US</h3><br>
+          
+              <div class="row">
+                 <div class="col-sm-3"><a href="#"><i class="fa fa-facebook"></i></a></div>
+                 <div class="col-sm-3"><a href="#"><i class="fa fa-instagram"></i></a></div>
+                 <div class="col-sm-3"><a href="#"><i class="fa fa-twitter"></i></a></div>
+                 <div class="col-sm-3"><a href="#"><i class="fa fa-youtube"></i></a></div>
+              </div>
+           
+        </div>
+      </div>
+    </div>
+    
+  </div>
+  <div class="panel-body footer-top ">
+      <div class="contanier">
+          <div class="row">
+              <div class="col-sm-3">
+                <h3>GET IN TOUCH WITH US</h3>
+                <div class="row">
+                    <div class="col-md-4">  <p>Address: </p></div>
+                    <div class="col-md-8"> <p> 49/1 alwisplace, kotahena,colombo 13</p></div>
+                </div>
+                <div class="row">
+                   <div class="col-md-4"><p>Phone: </p></div>
+                    <div class="col-md-8"><p>  (02) 9558 3116</p> </div>
+               </div>
+                <div class="row">
+                  <div class="col-md-4"><p>Email: </p> </div>
+                  <div class="col-md-8"><p>  sales@camera-warehouse.com.au</p> </div>
+                </div>
+                               
+              </div>
+              <div class="col-sm-3">
+              <h3>TOP CATEGORY</h3>
+                           
+                           <p><a href="#">SLR Lenses</a></p>
+                           <p><a href="#">Lens Filter</a></p>
+                           <p><a href="#">Digital Video Camera</a></p>
+                           <p><a href="#">Camera Bag</a></p>
+                           <p><a href="#">Binoculars</a></p>
+                           <p><a href="#">Memory Card</a></p>
+                           <p><a href="#">Gift Vouchers</a></p>
+                           <p><a href="#">Interest Free Camera Finance</a></p>
+              </div>
+              <div class="col-sm-3">
+              <h3>CUSTOMER SERVICE</h3>
+                           
+                           <p><a href="#">Sitemap</a></p>
+                           <p><a href="#">Help & FAQ</a></p>
+                           <p><a href="#">About Us</a></p>
+                           <p><a href="#">Feedback</a></p>
+                           <p><a href="#">Contact Us</a></p>
+                           <p><a href="#">My Compare Products</a></p>
+                           <p><a href="#">Blog/News</a></p>
+              </div>
+              <div class="col-sm-3">
+              <h3>POLICIES</h3>
+                           
+                           <p><a href="#">Shipping Policy</a></p>
+                           <p><a href="#">Return Policy</a></p>
+                           <p><a href="#">Disclaimer</a></p>
+                           <p><a href="#">Privacy Policy</a></p>
+                           <p><a href="#">Terms Of Use</a></p>
+              </div>
+          </div>
+      </div>
+  </div>
+  <p class="footer-bottom-text">All Right reserved by @copy;Divinector.2020   </P>
+</div>
---
+++ resources/views/master.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>E-Commerce Project</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  
+
+
+</head>
+<body>
+    {{View::make('header')}}
+    @yield('content')
+    {{View::make('footer')}}
+</body>
+<style>
+    .custom-login{
+        height:500px;
+        padding-top:100px;
+    }
+    .custom-panel{
+        background-color:#000;
+        color:#fff;
+    }
+    .custom-submit{
+        background: #52160a;
+        border: none;
+    }
+    .footer-top {
+        background: #111;
+        color:#fff;
+    }
+    .footer-bottom-text {
+        text-align: center;
+        background: #000;
+        line-height: 75px;
+        color:#fff;
+    }
+</style>
+
+</html>
